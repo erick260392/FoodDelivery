932781c5d5f11d5c79a5bf07105141166f1febb8
erick260392 <erickliconamorales2016@gmail.com>
2024-02-19 13:52:35 -0600

    navBar

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "./Navbar";
+
+
+function Home() {
+    return (
+        <div className='home-container'>
+            <Navbar />
+
+        </div>
+    )
+}
+
+export default Home
